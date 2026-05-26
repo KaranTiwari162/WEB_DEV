dd5f4d587298346dd1c10ec8aafc261f634064ae
webkaran18 <webdevkaran18@gmail.com>
2026-05-26 22:23:58 +0530

    practice dom

--- practice set 3/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-let marks = {
-    Harry : 98,
-    Rohan : 70,
-    Karan : 7
-}
-for (let i = 0; i < Object.keys(marks).length; i++) {
-   let key = Object.keys(marks)[i];
-
-  console.log(key + " marks are " + marks[key]); }
